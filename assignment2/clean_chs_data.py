46b5a6bca96ad79894c4fd126e0071cf69b454ea
botanyse <toramancansu@gmail.com>
2023-11-13 21:49:53 +0100

    Added assignment 2 folder.

---
+++ assignment2/clean_chs_data.py
@@ -0,0 +1,9 @@
+def clean_chs_data(raw):
+    pass
+
+
+if __name__ == "__main__":
+    # load the raw dataset
+    # call the cleaning function
+    # save the clean dataset in the bld folder
+    pass
---
+++ assignment2/clean_nlsy_data.py
@@ -0,0 +1,14 @@
+def clean_and_reshape_nlsy_data(raw, info):
+    pass
+
+
+def clean_year_data(raw, year, info):
+    pass
+
+
+if __name__ == "__main__":
+    # load the raw dataset
+    # load the info
+    # call the cleaning function
+    # save the clean dataset in the bld folder
+    pass
