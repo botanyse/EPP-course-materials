10c47dcfd3de37e7c2d43f18265fdd3d9b8b9b8a
botanyse <toramancansu@gmail.com>
2023-11-14 20:51:25 +0100

    Modified clean_bpi and clean_chs_data functions.

--- assignment2/clean_chs_data.py
+++ assignment2/clean_chs_data.py
@@ -10,16 +10,16 @@ bld = this_file_dir / "bld"
 data_path = bld / "chs_data.dta"
 
 def clean_chs_data(raw):
-    df_index = [_clean_momid_and_childid(raw["childid"]), raw["year"]]
-    df = pd.DataFrame(index = df_index)
+    df = pd.DataFrame(index = raw.index)
     for i in ["bpiA", "bpiB", "bpiC", "bpiD", "bpiE"]:
         df[i] = _clean_bpi(raw[i])
     df["momid"] = _clean_momid_and_childid(raw["momid"])
     df["age"] = raw["age"]
+    df = df.set_index([_clean_momid_and_childid(raw["childid"]), raw["year"]])
     return df
 
 def _clean_bpi(sr):
-    sr = sr.replace({sr[i]: pd.NA for i in sr.index if sr[i]<0})
+    sr = sr.replace({-100.000000: pd.NA})
     return sr
 
 def _clean_momid_and_childid(sr):
@@ -28,4 +28,5 @@ def _clean_momid_and_childid(sr):
 if __name__ == "__main__":
     raw = pd.read_stata(data_path)
     df = clean_chs_data(raw)
-    df.to_feather(bld / "clean_chs_data.feather")
+    df.to_csv(bld / "clean_chs_data.csv")
+
