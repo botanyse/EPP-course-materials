23cdab77c001ce048084712c3480b623d1def7b6
botanyse <toramancansu@gmail.com>
2023-11-16 23:03:13 +0100

    Worked on nlsy data.

--- assignment2/clean_nlsy_data.py
+++ assignment2/clean_nlsy_data.py
@@ -1,14 +1,42 @@
+import pandas as pd
+from pathlib import Path
+
+pd.options.mode.copy_on_write = True
+pd.options.future.infer_string = True
+pd.options.plotting.backend = "plotly"
+
+this_file_dir = Path(__file__).parent
+bld = this_file_dir / "bld"
+
 def clean_and_reshape_nlsy_data(raw, info):
     pass
 
+def clean_year_data(raw, info):
+    df = pd.DataFrame(index=raw.index)
+    df = _clean_bpi_variables(raw,info)
+    for i in df.columns[:2]:
+        df[i]= df[i].astype(pd.Int32Dtype()).astype(pd.CategoricalDtype())
+    for i in df.columns[3:]:
+        df[i] = _clean_bpi_cat(df[i])
+    return df
 
-def clean_year_data(raw, year, info):
-    pass
+def _clean_bpi_variables(raw_df, info_df):
+     raw_df = raw_df[info_df["nlsy_name"]]  # choosing variables that are available in info 
+     clean_variables = dict(zip(info_df.nlsy_name, info_df.readable_name +  ' ' + info_df.survey_year)) # creating a dictionary to rename columns in raw data
+     return raw_df.rename(columns=clean_variables)
 
-
+def _clean_bpi_cat(sr):
+    sr = sr.replace([-7.0, -3.0, -2.0, -1.0], pd.NA)
+    sr = sr.replace({'Never Attended School': pd.NA, 'Multiple selection': pd.NA })
+    categories = ["not true", "sometimes true", "often true"]
+    sr = sr.astype(pd.StringDtype()).str.lower().astype(pd.CategoricalDtype(categories=categories, ordered=True))
+    return sr
+    
 if __name__ == "__main__":
-    # load the raw dataset
-    # load the info
-    # call the cleaning function
-    # save the clean dataset in the bld folder
-    pass
+    raw = pd.read_stata(bld / "BEHAVIOR_PROBLEMS_INDEX.dta")
+    info = pd.read_csv(bld / "bpi_variable_info.csv")
+    clean_year_data(raw, info)
+    df = clean_year_data(raw,info)
+    df
+   #  df.to_csv(bld / "clean_year_date.csv")
+
