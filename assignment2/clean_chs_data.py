10f8dcd488acb4f526ab6726933ca94bdd8b9254
botanyse <toramancansu@gmail.com>
2023-11-18 17:14:20 +0100

    Finished Task 5 and 6.

--- assignment2/clean_chs_data.py
+++ assignment2/clean_chs_data.py
@@ -9,21 +9,22 @@ this_file_dir = Path(__file__).parent
 bld = this_file_dir / "bld"
 data_path = bld / "chs_data.dta"
 
-def clean_chs_data(raw):
+def clean_chs_data(raw): 
     df = pd.DataFrame(index = raw.index)
     for i in ["bpiA", "bpiB", "bpiC", "bpiD", "bpiE"]:
         df[i] = _clean_bpi(raw[i])
-    df["momid"] = _clean_momid_and_childid(raw["momid"])
-    df["age"] = raw["age"]
-    df = df.set_index([_clean_momid_and_childid(raw["childid"]), raw["year"]])
+    df["momid"] = _change_data_types(raw["momid"])
+    df["age"] = _change_data_types(raw["age"])
+    df = df.set_index([_change_data_types(raw["childid"]), _change_data_types(raw["year"])])
+    df = df.reindex(['momid', 'age', "bpiA", "bpiB", "bpiC", "bpiD", "bpiE" ], axis=1)
     return df
 
 def _clean_bpi(sr):
     sr = sr.replace({-100.000000: pd.NA})
     return sr
 
-def _clean_momid_and_childid(sr):
-    return sr.astype(pd.Int32Dtype()).astype(pd.CategoricalDtype())
+def _change_data_types(sr):
+    return sr.astype(pd.UInt32Dtype())
 
 if __name__ == "__main__":
     raw = pd.read_stata(data_path)
--- assignment2/clean_nlsy_data_2.py
+++ assignment2/clean_nlsy_data_2.py
@@ -9,28 +9,38 @@ this_file_dir = Path(__file__).parent
 bld = this_file_dir / "bld"
 
 def clean_and_reshape_nlsy_data(raw, info):
-    pass
+
+    cleaned_yearly_data = [clean_year_data(raw,i, info) for i in range(1986,2011,2)]
+    return pd.concat(cleaned_yearly_data)
 
 def clean_year_data(raw, year, info):
     df = pd.DataFrame(index=raw.index)
-    df = _clean_bpi_variables(raw,info)
-    for i in df.columns[:2]:
-        df[i]= df[i].astype(pd.Int32Dtype()).astype(pd.CategoricalDtype())
-    for i in df.columns[3:]:
+    for i in raw.columns[:3]:
+        df[i] = raw[i]
+    year_list = _filter_by_year(raw,year,info)
+    df[year_list] = raw[year_list]
+    df['year'] = year
+    df = _clean_bpi_variables(df,info)
+    df["childid"] = _change_data_types(df["childid"])
+    df["momid"] = _change_data_types(df["momid"])
+    df  = df.set_index(["childid","year"])
+    for i in df.columns[2:]:
         df[i] = _clean_bpi_cat(df[i])
     
-    df = pd.wide_to_long(df, stubnames= info.readable_name.unique(),i= 'childid invariant', j='year', sep= " ")
-    df = df.drop(columns=['childid','momid','birth_order'])
-    df.index.names = ['childid', 'year']
-    df = df.rename(columns= {'momid invariant': 'momid', 'birth_order invariant': 'birth_order'})
-    df = df.sort_index()
     df = pd.concat([df, _add_subscale_scores(df)], axis=1)
-    return df.xs(year, level=1)
+    return df.sort_index()
 
-def _clean_bpi_variables(raw_df, info_df):
-     raw_df = raw_df[info_df["nlsy_name"]]  # choosing variables that are available in info 
-     clean_variables = dict(zip(info_df.nlsy_name, info_df.readable_name +  ' ' + info_df.survey_year)) # creating a dictionary to rename columns in raw data
-     return raw_df.rename(columns=clean_variables)
+def _change_data_types(sr):
+    return sr.astype(pd.UInt32Dtype())
+
+def _filter_by_year(raw_df,year,info):
+    info_by_year = info.loc[info.survey_year == str(year)]
+    nlsy_by_year = info_by_year.loc[:,"nlsy_name"].to_list()
+    return nlsy_by_year
+
+def _clean_bpi_variables(df, info_df):
+     clean_variables = dict(zip(info_df.nlsy_name, info_df.readable_name)) # creating a dictionary to rename columns in raw data
+     return df.rename(columns=clean_variables)
 
 def _clean_bpi_cat(sr):
     sr = sr.replace([-7.0, -3.0, -2.0, -1.0], pd.NA)
@@ -50,19 +60,13 @@ def _add_subscale_scores(df):
         subscale[i] = subscale[i].map(mapping_dict)
     
     categories = ["antisocial", "anxiety", "headstrong", "hyperactive", "dependence","peer"]
-
     for i in categories:
         subscale[i] = subscale[[col for col in subscale.columns if col.startswith(i)]].mean(axis=1)
-    
     subscale = subscale[categories]
-
     return subscale
 
-  
 if __name__ == "__main__":
     raw = pd.read_stata(bld / "BEHAVIOR_PROBLEMS_INDEX.dta")
     info = pd.read_csv(bld / "bpi_variable_info.csv")
-    
-    df = clean_year_data(raw,1998,info)
-    df
-   #  df.to_csv(bld / "clean_year_date.csv")
+    df = clean_and_reshape_nlsy_data(raw,info)
+    df.to_csv(bld / "clean_nlsy_data.csv")
